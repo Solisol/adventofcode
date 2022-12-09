ea09ebe1738482db4914572082d0aee1acfd0a46
Jessica Andersson <jesandeit@gmail.com>
2022-12-09 16:20:32 +0100

    add scanner to copy paste base line

--- 2022/Main.java
+++ 2022/Main.java
@@ -1,5 +1,20 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello Day ");
+        try {
+            Scanner scanner = new Scanner(new File("input/-test.txt"));
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+            }
+            scanner.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
